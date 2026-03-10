e2afbed9a62b9772d6b5321c4f75f0e1c3fe4cef
Olivier <olivier.roche@adaptive-ml.com>
2026-03-10 11:49:21 +0100

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from test-recipe-git!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ recipes/test.py
@@ -0,0 +1,13 @@
+from harmony_client.runtime import recipe_main
+from pydantic import BaseModel
+from harmony_client.runtime import RecipeContext
+
+
+class Config(BaseModel):
+    name: str
+    age: int
+
+
+@recipe_main
+def main(config: Config, ctx: RecipeContext):
+    print(f"Hello {config.name}! You are {config.age} years old.")
