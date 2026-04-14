2d0ec584a8c1fee2fb81b6f1f95675bffd51aa92
Olivier <olivier.roche@adaptive-ml.com>
2026-04-14 16:15:31 +0200

    add new recipe

---
+++ recipes/test-4.py
@@ -0,0 +1,13 @@
+from harmony_client.runtime import recipe_main, InputConfig
+from pydantic import BaseModel
+from harmony_client.runtime import RecipeContext
+
+
+class ConfigBis(InputConfig):
+    country: str
+    size: int
+
+
+@recipe_main(name="great recipe", description="a great recipe")
+def great_recipe(config: ConfigBis, ctx: RecipeContext):
+    print(f"Hello user from {config.country}! You are {config.int} tall.")
