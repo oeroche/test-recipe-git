a97930e975fc160e36784e6189e89e1982d6d6bd
Olivier <olivier.roche@adaptive-ml.com>
2026-04-16 15:17:31 +0200

    restructure

---
+++ src/nested/folder/nested_recipe.py
@@ -0,0 +1,22 @@
+from harmony_client.runtime import recipe, InputConfig
+from harmony_client.runtime import RecipeContext
+
+
+class MyConfig(InputConfig):
+    name: str
+    age: int
+
+
+class MyOtherConfig(InputConfig):
+    country: str
+    size: int
+
+
+@recipe
+def main(config: MyConfig, ctx: RecipeContext):
+    print(f"Hello {config.name}! You are {config.age} years old.")
+
+
+@recipe(name="Nested Recipe", description="A nested recipe")
+def other(config: MyOtherConfig, ctx: RecipeContext):
+    print(f"Hello user from {config.country}! You are {config.int} tall.")
