c4962c3e0d8670eccdea3c53bafb6f5891adc7f7
Olivier Roche <addict06@gmail.com>
2026-05-26 16:22:10 +0200

    Remove main and new_recipe functions
    Removed main and new_recipe functions, keeping only other.

--- src/nested/folder/nested_recipe.py
+++ src/nested/folder/nested_recipe.py
@@ -13,22 +13,9 @@ class MyOtherConfig(InputConfig):
     size: int
 
 
-@recipe
-def main(config: MyConfig, ctx: RecipeContext):
-    print(f"Hello {config.name}! You are {config.age} years old.")
-
-
 @recipe(
     name="Nested Recipe",
     description="A nested recipe",
 )
 def other(config: MyOtherConfig, ctx: RecipeContext):
     print(f"Hello user from {config.country}! You are {config.int} tall.")
-
-
-@recipe(
-    name="A new recipe has been created",
-    description="A new recipe has been created",
-)
-def new_recipe(config: MyConfig, ctx: RecipeContext):
-    print(f"Hello {config.name}! You are {config.age} years old from a new recipe")
